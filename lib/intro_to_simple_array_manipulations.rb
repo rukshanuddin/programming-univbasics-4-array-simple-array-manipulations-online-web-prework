eda52252c713c455c5908c59fb7de561ac6b6536
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-03 17:04:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,3 +21,10 @@ end
 def shift_with_args(array)
   array.shift(2)
 end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element) 
+  array[3].insert(element) 
